d87d77c45485437b363d36b4655675a0a147987a
rossrobino <rbrobino@gmail.com>
2023-01-04 17:01:48 -0800

    github api post

---
+++ src/components/GitHubApiLink.svelte
@@ -0,0 +1,21 @@
+<script>
+	let input = "rossrobino";
+</script>
+
+<input type="text" placeholder="username" class="my-2" bind:value={input} />
+
+<div>
+	<a href="https://api.github.com/users/{input}">
+		{`https://api.github.com/users/${input}`}
+	</a>
+</div>
+<div>
+	<a href="https://api.github.com/users/{input}/repos">
+		{`https://api.github.com/users/${input}/repos`}
+	</a>
+</div>
+<div>
+	<a href="https://api.github.com/users/{input}/followers">
+		{`https://api.github.com/users/${input}/followers`}
+	</a>
+</div>
