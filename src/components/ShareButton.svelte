6c675d7c5d192f4cfc4d8f929c821ceb700680d2
rossrobino <rbrobino@gmail.com>
2023-01-07 08:15:48 -0800

    imported share button

--- src/components/ShareButton.svelte
+++ src/components/ShareButton.svelte
@@ -1,34 +1,10 @@
 <script>
-	export let text = "Check out this post:";
-	export let domain = "https://blog.robino.dev";
-	export let path = "";
-	export let title = path.split("/").splice(-1); // default to end of path
-
-	let url = domain + path;
-
-	let complete = false;
-
-	async function handleClick() {
-		try {
-			if (navigator.canShare) {
-				await navigator.share({ title, text, url });
-			} else {
-				await navigator.clipboard.writeText(url);
-				complete = true;
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	}
+	import { ShareButton } from "@rossrobino/components";
+	export let url = "";
 </script>
 
-<button
-	on:click={handleClick}
+<ShareButton
+	{url}
+	text="Check out this blog post:"
 	class="bg-emerald-800 text-neutral-50 p-3 my-4 w-32 rounded-sm"
->
-	{#if complete}
-		Copied!
-	{:else}
-		Share
-	{/if}
-</button>
+/>
