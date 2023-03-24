7b45e0fdd3011b25b7ad33660e872c376ec29fe6
rossrobino <rbrobino@gmail.com>
2023-03-24 10:27:42 -0700

    2.0 maintenance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,30 +2,19 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
-
-// https://astro.build/config
 import partytown from "@astrojs/partytown";
-
-// https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
 	markdown: {
 		shikiConfig: {
 			theme: "github-dark",
 		},
 	},
-	site: "https://blog.robino.dev",
+	site: "https://blog.robino.dev/",
 	integrations: [
 		tailwind({
 			config: {
