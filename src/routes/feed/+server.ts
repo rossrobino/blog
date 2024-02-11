ac1430c29e1f4157745936545d0ea0bcca67e28f
rossrobino <rbrobino@gmail.com>
2024-02-10 19:56:45 -0500

    fix rss links

--- src/routes/feed/+server.ts
+++ src/routes/feed/+server.ts
@@ -17,16 +17,16 @@ export const GET = async () => {
 const xml = (posts: Post[]) => `<?xml version="1.0" encoding="UTF-8" ?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
-<atom:link href="http://robino.dev/feed" rel="self" type="application/rss+xml" />
+<atom:link href="http://blog.robino.dev/feed" rel="self" type="application/rss+xml" />
 <title>blog.robino.dev</title>
-<link>http://robino.dev</link>
+<link>http://blog.robino.dev</link>
 <description>Ross Robino's blog</description>
 ${posts
 	.map(
 		(post) => `<item>
-<guid>https://robino.dev/posts/${post.slug}</guid>
+<guid>https://blog.robino.dev/posts/${post.slug}</guid>
 <title>${post.title}</title>
-<link>https://robino.dev/posts/${post.slug}</link>
+<link>https://blog.robino.dev/posts/${post.slug}</link>
 <description>${post.description}</description>
 <pubDate>${new Date(post.date).toUTCString()}</pubDate>
 </item>`,
