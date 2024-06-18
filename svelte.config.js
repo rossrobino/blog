b9f37532ce029d5008bdbf724ec90a256fd1f05b
rossrobino <rbrobino@gmail.com>
2024-06-18 11:39:00 -0400

    update adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	kit: { adapter: adapter() },
+	kit: { adapter: adapter({ runtime: "edge" }) },
 	vitePlugin: {
 		inspector: true,
 	},
