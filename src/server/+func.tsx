1842ddb26fbe8fc7f3513a5c71ba118884c00e7b
rossrobino <rbrobino@gmail.com>
2025-02-13 14:17:24 -0500

    rm log

--- src/server/+func.tsx
+++ src/server/+func.tsx
@@ -15,8 +15,6 @@ const posts = (await getPosts()).filter(
 export const handler: Handler = async (req) => {
 	const url = new URL(req.url);
 
-	console.log(url);
-
 	const page = new Injector(html);
 
 	if (url.pathname === "/") {
