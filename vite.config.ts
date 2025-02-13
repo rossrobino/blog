6c6dc8c7ce5e7d1f78e4e0ea063b51a71ef3c99e
rossrobino <rbrobino@gmail.com>
2025-02-13 12:40:26 -0500

    vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
+import { adapter } from "@domcojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { domco } from "domco";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [domco(), tailwindcss()],
+	plugins: [
+		domco({
+			adapter: adapter({
+				isr: {
+					expiration: false,
+				},
+			}),
+		}),
+		tailwindcss(),
+	],
 });
