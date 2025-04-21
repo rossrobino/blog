f37cfd4225a0c09c82c38f64dfc740a79519760b
rossrobino <rbrobino@gmail.com>
2025-04-21 13:55:00 -0400

    undo

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,2 @@
 /// <reference types="vite/client" />
 /// <reference types="domco/env" />
-
-interface ImportMetaEnv {
-	readonly UPSTASH_REDIS_REST_URL?: string;
-	readonly UPSTASH_REDIS_REST_TOKEN?: string;
-}
