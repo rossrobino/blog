c94797da8ea3037d94e96cbf377a85ccab779ad0
rossrobino <rbrobino@gmail.com>
2025-04-21 13:37:25 -0400

    rm isr

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,7 @@
 /// <reference types="vite/client" />
 /// <reference types="domco/env" />
+
+interface ImportMetaEnv {
+	readonly UPSTASH_REDIS_REST_URL?: string;
+	readonly UPSTASH_REDIS_REST_TOKEN?: string;
+}
