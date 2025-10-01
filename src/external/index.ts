bbe671fc3950f89bb5ba4ac2c37a2c64bc8a5c33
rossrobino <rbrobino@gmail.com>
2025-10-01 16:37:22 -0400

    skip link

--- src/external/index.ts
+++ src/external/index.ts
@@ -8,4 +8,12 @@ export const external: Post[] = [
 		date: "Jul 8, 2025",
 		slug: "https://ovr.robino.dev",
 	},
+	{
+		title: "Create a Skip Link with TailwindCSS",
+		description:
+			"Create a link for screen reader users to skip to the main content of your webpage.",
+		keywords: ["html", "css", "tailwind"],
+		date: "Oct 1, 2025",
+		slug: "https://drab.robino.dev/styles/skiplink/",
+	},
 ];
